e5d858ff749573068e02e6231bebff56d39b248e
quentinlaurent <quentin.laurent.sup@gmail.com>
2023-06-04 20:03:16 +0200

    added FAQ link to footer

--- src/view/footer.php
+++ src/view/footer.php
@@ -11,6 +11,7 @@ $imgDirectory = "http://$hostname/" . ROOT_URI . '/static/img/';
     <div id="footer-links">
         <a class="link" href="#">Mentions légales</a>
         <a class="link" href="#">CGU</a>
+        <a class="link" href="#">FAQ</a>
         <a class="link" href="#">Contact</a>
     </div>
     <div></div>
